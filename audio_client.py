65d793e63f4089e6896d3aa98a25f807ab47d3c0
HarisNaveed17 <haris.naveed174@gmail.com>
2021-12-21 14:10:19 +0500

    use scipy_io to read/write

--- audio_client.py
+++ audio_client.py
@@ -1,9 +1,9 @@
 import grpc
 import AudioSender_pb2 as service
 import AudioSender_pb2_grpc as rpc
-import wave
 import base64
 import os
+from scipy.io.wavfile import read
 
 FILES_TO_SEND = '/home/haris/code/data/files_to_send'
 MAX_MESSAGE_LENGTH = 104857600
@@ -12,15 +12,18 @@ MAX_MESSAGE_LENGTH = 104857600
 def encode_audio(FILES_TO_SEND):
     audio_list = []
     for filename in os.listdir(FILES_TO_SEND):
-        with wave.open(FILES_TO_SEND + '/' + filename, 'rb') as to_encode:
-            length = to_encode.getnframes()
-            frames = to_encode.readframes(length)
-            encoded_frames = base64.b64encode(frames)
-
-        Audio = service.Audio(audio=encoded_frames,
-                              ID=filename)
+        file_path = f'{FILES_TO_SEND}/{filename}'
+        sr, data = read(file_path)
+        encoded_data = base64.b64encode(data)
+        Audio = service.Audio(audio=encoded_data,
+                              ID=filename, samplerate=sr)
         audio_list.append(Audio)
     return audio_list
+
+
+def generate_stream(audio_files):
+    for file in audio_files:
+        yield file
 
 
 def run():
@@ -29,7 +32,9 @@ def run():
     channel = grpc.insecure_channel('localhost:5005', options=[('grpc.enable_http_proxy', 0),
                                                                ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH), ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH), ],)
     stub = rpc.AudioSenderStub(channel)
-    audio_iterator = encode_audio(FILES_TO_SEND)
+    encoded_audios = encode_audio(FILES_TO_SEND)
+    audio_iterator = generate_stream(encoded_audios)
+
     say = stub.SendAudio(audio_iterator)
     print(say)
 
